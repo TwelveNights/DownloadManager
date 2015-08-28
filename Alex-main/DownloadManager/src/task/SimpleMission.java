e3313eccc2a0d7605f3da2b5a2a263adbd7f473b
Alex Cao <e90mc2@gmail.com>
2015-08-28 00:06:20 -0700

    Complete Javadoc for Mission and SimpleMission classes. Enhanced thread safety for said classes.

--- Alex-main/DownloadManager/src/task/SimpleMission.java
+++ Alex-main/DownloadManager/src/task/SimpleMission.java
@@ -12,26 +12,62 @@ public class SimpleMission extends Mission {
 
 	private static final long serialVersionUID = 5113449782029278939L;
 
+	/**
+	 * Constructs a SimpleMission with the specified URL and Path.
+	 * 
+	 * @param url
+	 *            The URL from which this mission downloads a file.
+	 * @param path
+	 *            The Path to which the downloaded file is stored to. File name
+	 *            must be included.
+	 */
 	public SimpleMission(URL url, Path path) {
 		this.url = url;
 		this.path = path;
 	}
 
+	/**
+	 * The active thread (if any) dedicated for this download mission.
+	 */
 	transient Thread t;
 
-	// Variables representing progress in byte
+	/**
+	 * Download progress represented in number of bytes downloaded
+	 */
 	long current = 0;
 	long total = -1;
 
+	/**
+	 * Starts the download mission by creating a new thread and activates it.
+	 */
 	public void start() {
+		this.inProgress = true;
 		t = new Thread(new SimpleTask());
 		t.start();
-		this.inprogress = true;
 	}
-	
+
+	/**
+	 * Stops the active thread. Do nothing if no thread is active or said thread
+	 * is not initialized. Note that said thread is not immediately stopped.
+	 * inProgress() or join() must be called to ensure that said thread is
+	 * safely stopped.
+	 */
 	public void pause() {
-		t.interrupt();
-		this.inprogress = false;
+		if (this.inProgress && t != null)
+			t.interrupt();
+	}
+
+	/**
+	 * Wait for the active thread (if any) to die.
+	 * 
+	 * @throws InterruptedException
+	 *             if any thread has interrupted the current thread. The
+	 *             interrupted status of the current thread is cleared when this
+	 *             exception is thrown.
+	 */
+	public void join() throws InterruptedException {
+		if (this.inProgress && t != null)
+			t.join();
 	}
 
 	private class SimpleTask implements Runnable {
@@ -44,12 +80,14 @@ public class SimpleMission extends Mission {
 				if (total == -1) {
 					total = conn.getContentLengthLong();
 				} else {
-					conn.setRequestProperty("Range", "Bytes="+current+"-");
+					// TODO casing by protocol
+					conn.setRequestProperty("Range", "Bytes=" + current + "-");
 				}
 
-				if (Thread.interrupted())
+				if (Thread.interrupted()) {
 					return;
-				
+				}
+
 				try (InputStream in = conn.getInputStream();
 						FileOutputStream out = new FileOutputStream(path.toFile());) {
 
@@ -58,9 +96,10 @@ public class SimpleMission extends Mission {
 
 					while ((len = in.read(buf)) != -1) {
 						out.write(buf, 0, len);
-						total += len;
-						if (Thread.interrupted())
+						current += len;
+						if (Thread.interrupted()) {
 							return;
+						}
 					}
 
 				} catch (FileNotFoundException e) {
@@ -77,6 +116,8 @@ public class SimpleMission extends Mission {
 				// TODO
 				e.printStackTrace();
 				System.out.println("Failed to establish connection");
+			} finally {
+				inProgress = false;
 			}
 
 		}
