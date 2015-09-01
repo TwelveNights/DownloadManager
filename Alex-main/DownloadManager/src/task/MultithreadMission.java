f1f61f18b745a738e6162daf02acecaa53817f04
Alex Cao <e90mc2@gmail.com>
2015-09-01 16:04:42 -0700

    Adding a new class for multithreaded download (unfinished)

---
+++ Alex-main/DownloadManager/src/task/MultithreadMission.java
@@ -0,0 +1,35 @@
+package task;
+
+public class MultithreadMission extends Mission {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void join() throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getTotalSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getCurrentSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
