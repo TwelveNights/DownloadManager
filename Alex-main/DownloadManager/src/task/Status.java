e10d431f61c0936994dabcfedd33aa4fc6ceea8e
TwelveNights <eddiezhou1996@gmail.com>
2015-09-09 10:46:39 -0700

    Cleanup on Aisle 4

--- Alex-main/DownloadManager/src/task/Status.java
+++
@@ -1,5 +0,0 @@
-package task;
-
-public enum Status {
-	IN_PROGRESS, NOT_STARTED, PAUSED, FINISHED, FAILED
-}
---
+++ src/task/Status.java
@@ -0,0 +1,5 @@
+package task;
+
+public enum Status {
+	IN_PROGRESS, NOT_STARTED, PAUSED, FINISHED, FAILED
+}
